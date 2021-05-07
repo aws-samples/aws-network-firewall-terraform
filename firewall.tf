c2aa7c17686b4e85952988a3ceb94d96d270976a
Nicola Arnoldi <narnoldi@amazon.com>
2021-05-07 16:21:30 +0100

    remove wrong KMS key from S3 bucket configuration

--- firewall.tf
+++ firewall.tf
@@ -152,7 +152,6 @@ resource "aws_s3_bucket" "anfw_flow_bucket" {
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
-        kms_master_key_id = "aws/s3"
         sse_algorithm     = "aws:kms"
       }
     }
