497f8a06718598b7e8acbaa609d8d8623d2a7579
Nicola Arnoldi <narnoldi@amazon.com>
2021-06-04 12:47:43 +0100

    change S3 encryption as KMS not supported

--- firewall.tf
+++ firewall.tf
@@ -152,7 +152,7 @@ resource "aws_s3_bucket" "anfw_flow_bucket" {
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
-        sse_algorithm     = "aws:kms"
+        sse_algorithm     = "AES256"
       }
     }
   }
